cdfb6af807d5ea15c15b0d495f0540bb5fcb5b3d
Davis Kimoto <davis@kimoto.dev>
2024-06-13 06:11:47 -0600

    chore: remove phoenix app

--- lib/ark/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Ark.Mailer do
-  use Swoosh.Mailer, otp_app: :ark
-end
--- lib/ark/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Ark.Repo do
-  use Ecto.Repo,
-    otp_app: :ark,
-    adapter: Ecto.Adapters.Postgres
-end
